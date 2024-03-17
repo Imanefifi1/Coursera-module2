9e7230e58200b939e6abe166249f21363d1fe8f8
imane hachefi <imanehachefi1998@gmail.com>
2024-03-17 21:46:26 +0000

    feat(first-commit) : initialisation of the project

---
+++ test.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+# include <stdlib.h>
+int foo (int q) {
+    int x = 1;
+    return (q + x);
+}
+int main (){
+   int x = 0;
+   while (x < 3) {
+  	printf ("%i ", x);
+      x = x + foo(x);
+    }
+}
